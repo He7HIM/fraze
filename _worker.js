098a5ad85abb3b43cfc970d3b0fd6a748d22d33b
He7HIM <he7him777@gmail.com>
2025-10-06 23:26:21 +0400

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -4,7 +4,7 @@ import { connect } from "cloudflare:sockets";
 
 // Variables
 const rootDomain = "topnet7hackers.space"; // Ganti dengan domain utama kalian
-const serviceName = "TOpNeT"; // Ganti dengan nama workers kalian
+const serviceName = "topnet"; // Ganti dengan nama workers kalian
 const apiKey = ""; // Ganti dengan Global API key kalian (https://dash.cloudflare.com/profile/api-tokens)
 const apiEmail = ""; // Ganti dengan email yang kalian gunakan
 const accountID = ""; // Ganti dengan Account ID kalian (https://dash.cloudflare.com -> Klik domain yang kalian gunakan)
